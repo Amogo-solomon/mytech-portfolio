1856f07ab10ceb6f709c148156c27478647da0b6
Amogo Solomon <amogoagaba@gmail.com>
2023-10-10 17:22:53 +0100

    update

--- src/components/common/DownloadPDF.jsx
+++ src/components/common/DownloadPDF.jsx
@@ -6,9 +6,9 @@ export const handleDownloadResume = () => {
     
    {/* create  a link element */}
   const link = document.createElement("a");
-  link.href = "/resume_Amogo_Solomon.pdf"; {/* Replace with the actual path to ou PDF file */}
+  link.href = "/resume-Amogo-Solomon.pdf"; {/* Replace with the actual path to ou PDF file */}
   link.target = "_blank";
-  link.download = "resume_Amogo_Solomon.pdf"; {/* set the default download filename */}
+  link.download = "resume-Amogo-Solomon.pdf"; {/* set the default download filename */}
   
  {/*  Trigger a click event to start the download */}
   link.click();
